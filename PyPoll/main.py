a0943ca3d38b303df20487fea97dfae507f96667
Franch Fry <crfranchi93@gmail.com>
2019-02-08 22:57:55 -0800

    ninth commit

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -4,19 +4,20 @@ import csv
 
 #Read through the resource csv file titled "election_data.csv"
 election_data = os.path.join("election_data.csv")
+election_analysis = os.path.join("election_analysis.txt")
 
 #Open the CSV File & make sure new line is an empty space
 with open(election_data, 'r') as csvfile:
     #read through the csvfile and separate the three columns with a "," as the delimiter
     csvreader = csv.reader(csvfile, delimiter=',')
-    #Initialize variable csvheader and use the next function to skip headers in csvfile
+    # Read the header
     csvheader = next(csvreader)
     
     #set each variable to an index of zero
     total_votes= 0
     candidatevotes_won= 0
     candidatevotes_lost= 0
-    election_winner= 0
+    election_winner= ''
     #Variables for each candidate's name:
     Khan = 0
     Correy = 0
@@ -25,10 +26,14 @@ with open(election_data, 'r') as csvfile:
 
     #Complete a list of candidates who received votes
     Candidates = ["Khan", "Correy", "Li", "O'Tooley"]
+    CandidateVotes = {}
 
     #Use Variables and Candidates List to count number of votes each candidate received...
     for row in csvreader: 
-        #Calculate total number of votes cast by counting the number of rows in the 'Voter ID' Column:
+        #Find the total vote count
+        total_votes += 1
+
+        #Calculate total number of votes cast for each candidate:
         if row[3] == Candidates[0]:
             Khan += 1
         elif row[3] == Candidates[1]:
@@ -38,12 +43,12 @@ with open(election_data, 'r') as csvfile:
         else row[3] == Candidates[3]:
             OTooley += 1
 
+    
     for candidate in Candidates:
         #Calculate percentage of votes each candidate won
-        if candidatevotes_won = []
+        if Khan >= (Candidates[1], Candidates[2], Candidates [3]):
+
         #Calculate the total number of votes each candidate won
-
-
 
     #print(f'{candidates["name"]}')
     for i in range(len(candidatelist)):
